e3d295e9d445df7a887a9d6fd6f83d4f4086551f
Damassa <felipelealdamasceno@pop-os.localdomain>
2023-01-04 11:28:39 -0300

    Ajustando a landing page (tirando image-thumbnail)

--- src/resources/views/components/cards/series.blade.php
+++ src/resources/views/components/cards/series.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ route('single', $serie->id) }}">
         <div class="w-64 h-90 py-3 mb-5 border-2 border-blue-700 rounded-lg shadow-md">
             <h3 class="text-center font-bold">{{ $serie->name }}</h3>
-            <img src="{{ asset('storage/defaults/cards-thumbnail.jpg') }}" />
+            {{-- <img src="{{ asset('storage/defaults/cards-thumbnail.jpg') }}" /> --}}
             <div class="text-center">
                 <img class="mx-auto d-block" src="{{ $serie->image }}" alt="Responsive image" width="200" height="100" />
             </div>
